886c1b90a810d79ffe928059b787ce5750b6f64e
VladKhramykh <vladkhramykh@mail.ru>
2018-09-26 00:04:43 +0300

    add OOP_3

---
+++ OOP_3/OOP_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Vector v1 = new Vector();
+            for(int i = 0; i < 10; i++)
+                v1[i] = rnd.Next(-100,100);          
+            v1.PrintArray();
+            Console.WriteLine("\n Сумма элементов: {0}",v1.SumArray());
+            Console.WriteLine(" Произведение элементов: {0}", v1.MulpiplyArray());
+            Console.WriteLine("\n--------------------------------");
+
+            Vector v2 = new Vector(8);
+            v2.PrintArray();
+            Console.WriteLine("\n Сумма элементов: {0}", v2.SumArray());
+            Console.WriteLine(" Произведение элементов: {0}", v2.MulpiplyArray());
+            Console.WriteLine("\n--------------------------------");
+
+            Vector v3 = new Vector(4,5,0,90,4);    
+            v3.PrintArray();
+            Console.WriteLine("\n Сумма элементов: {0}", v3.SumArray());
+            Console.WriteLine(" Произведение элементов: {0}", v3.MulpiplyArray());
+            Console.WriteLine("\n--------------------------------\n");
+
+            bool[] isNull = { v1.isNullInArray(), v2.isNullInArray(), v3.isNullInArray() };
+            for(int i = 0; i<isNull.Length; i++)
+            {
+                if (isNull[i] == true)
+                    Console.WriteLine("В {0} массив ноль! ", (i + 1));
+            }
+
+            int[] ArrayMin = { v1.GetMin(), v2.GetMin(), v3.GetMin() };
+            for (int i = 0; i < ArrayMin.Length; i++)
+                Console.WriteLine("Минимальный элемент {0} в массиве: {1}", i + 1, ArrayMin[i]);
+
+            //CloseClass CC1 = new CloseClass(); 
+
+            StaticClass SC1 = new StaticClass();
+            StaticClass SC2 = new StaticClass();
+            StaticClass SC3 = new StaticClass();
+            StaticClass SC4 = new StaticClass();
+            StaticClass SC5 = new StaticClass();
+            Console.WriteLine("Количество экземпляров со статическим коструктором: {0}", StaticClass.countSC);
+        }
+    }
+}
---
+++ OOP_3/OOP_3/Vector.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3
+{
+    class Vector
+    {
+        int[] array;
+        int status = 0;
+        int length = 0;
+        public readonly string myName = " Храмых Владислав";
+        Random rnd = new Random();
+
+        public Vector()
+        {
+            array = new int[10];
+            length = 10;
+            //for (int count = 0; count<length; count++)
+            //    array[count] = (rnd.Next(100)/13);    
+        }
+
+        public Vector(int size)
+        {
+            array = new int[size];
+            length = size;
+            for (int count = 0; count < length; count++)
+                array[count] = (rnd.Next(-100,100)/3);
+        }
+
+        public Vector(int Item1, int Item2, int Item3, int Item4, int size)
+        {
+            array = new int[] { Item1, Item2,Item3,Item4};
+            length = size;
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i < 0 && i > length)
+                    status = 100;
+                return status;
+            }
+            set
+            {
+                if (i >= 0 && i <= length)
+                {
+
+                    if (value <= 100 & value > -100)
+                        array[i] = value;
+                    else
+                        status = 200;
+                }
+                else
+                    status = 100;
+            }
+        }
+
+        public void PrintArray()
+        {
+            for (int count = 0; count < this.length; count++)
+                Console.Write(array[count]+" ");
+        }
+
+        public int SumArray()
+        {
+            int sum = 0;
+            for (int i = 0; i < this.length; i++)
+                sum += array[i];
+            return sum;
+        }
+
+        public int MulpiplyArray()
+        {
+            int Mul = 1;
+            for (int i = 0; i < this.length; i++)
+                Mul += array[i];
+            return Mul;
+        }
+
+        public bool isNullInArray()
+        { 
+            for(int i = 0; i<this.length; i++)
+            {
+                if (array[i] == 0)                
+                    return true;                                            
+            }
+            return false;
+        }
+        public int GetMin()
+        {
+            return array.Min();
+        }
+    }
+
+
+    class CloseClass
+    {
+        CloseClass()
+        {
+            this.index = 30;
+        }
+        public int index = 10;
+    }
+
+    class StaticClass
+    {
+        static public int countSC = 0;
+        static StaticClass()
+        {
+            countSC++;
+        }
+    }
+}
